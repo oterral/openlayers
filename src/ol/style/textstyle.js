356fceff4ca4b0a5b39cf088405d8228761afcb9
Tom Payne <twpayne@gmail.com>
2013-12-12 10:58:26 +0100

    Make options argument to ol.style.Text optional

--- src/ol/style/textstyle.js
+++ src/ol/style/textstyle.js
@@ -4,9 +4,11 @@ goog.provide('ol.style.Text');
 
 /**
  * @constructor
- * @param {ol.style.TextOptions} options Options.
+ * @param {ol.style.TextOptions=} opt_options Options.
  */
-ol.style.Text = function(options) {
+ol.style.Text = function(opt_options) {
+
+  var options = goog.isDef(opt_options) ? opt_options : {};
 
   /**
    * @type {string|undefined}
